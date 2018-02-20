6d51cd1e15d515e0a4273181ef3d4b1c7341b499
dz902 <dz902i@gmail.com>
2018-02-20 23:38:43 +0800

    :construction: minor fix on directory import

--- src/Schema/Music/index.ts
+++ src/Schema/Music/index.ts
@@ -1,19 +1,11 @@
 export * from './types';
 
-export * from './Container';
 export * from './Mark';
 export * from './Context';
 
 export * from './Articulation';
 export * from './Pitch';
 export * from './Chord';
-export * from './Measure';
 export * from './Rest';
-export * from './Part';
 export * from './Score';
-export { Direction } from './Direction';
-export { Tie } from './Direction';
-export { Slur } from './Direction';
-export { Dynamics } from './Direction';
-export { DynamicsTypes } from './Direction';
-export { uniq } from './Mark';
+export * from './Direction';
